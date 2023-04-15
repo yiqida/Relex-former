e49d5c52c66c59043a8d36475c960bc617a3aaa5
wgf <843292747@qq.com>
2023-04-15 10:00:10 +0800

    feat: dnd集成进去

--- src/pages/EditPage.vue
+++ src/pages/EditPage.vue
@@ -6,21 +6,73 @@
         <el-button type="">保存</el-button>
         <el-button>清空</el-button>
       </el-header>
-      <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
+      <el-main>
+        <el-row :gutter="formConf.gutter">
+          <el-form :size="formConf.size" :label-position="formConf.labelPosition" :disabled="formDisabled" :label-width="formConf.labelWidth + 'px'" :model="homeModel">
+            <Container @drop="onDrop" orientation="vertical">
+              <Draggable class="drop-list" v-for="(item,index) in list" :key="index">
+                <div class="drop-item">
+                  <span>{{item.name}}</span>
+                </div>
+              </Draggable>
+            </Container>
+          </el-form>
+        </el-row>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 <script>
 import { ElContainer, ElAside, ElHeader, ElMain, ElButton } from "element-plus";
+import { Container, Draggable } from "vue3-smooth-dnd";
+
+import { formConf } from "@/common/rootFormConfig";
+import { ref } from 'vue'
 export default {
   components: {
     ElContainer,
     ElAside,
     ElHeader,
     ElMain,
-    ElButton
+    ElButton,
+    Container,
+    Draggable,
   },
-  setup() {},
+  setup() {
+    const list = ref([
+      {
+        name: "红",
+      },
+      {
+        name: "绿",
+      },
+      {
+        name: "蓝",
+      },
+    ]);
+    function onDrop (dropResult) {
+      list.value = applyDrag(list.value, dropResult)
+    }
+    function applyDrag(arr, dragResult){
+      const { removedIndex, addedIndex, payload } = dragResult;
+
+      if (removedIndex === null && addedIndex === null) return arr;
+      const result = [...arr];
+      let itemToAdd = payload;
+      
+      if (removedIndex !== null) {
+        itemToAdd = result.splice(removedIndex, 1)[0];
+      }
+      if (addedIndex !== null) {
+        result.splice(addedIndex, 0, itemToAdd);
+      }
+      return result;
+    }
+    return {
+      formConf,
+      list,
+      onDrop
+    };
+  },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,20 @@ const { defineConfig } = require("@vue/cli-service");
 const AutoImport = require("unplugin-auto-import/webpack");
 const Components = require("unplugin-vue-components/webpack");
 const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
+const path = require("path");
+const resolve = (dir) => path.resolve(__dirname, dir);
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
+    resolve: {
+      // 设置别名
+      alias: {
+        "@": resolve("src"), // 这样配置后 @ 可以指向 src 目录
+        "common": resolve("src/common")
+      },
+    },
     plugins: [
       AutoImport({
         resolvers: [ElementPlusResolver()],
@@ -13,7 +23,7 @@ module.exports = defineConfig({
       Components({
         resolvers: [ElementPlusResolver()],
       }),
-      require('unplugin-element-plus/webpack')({
+      require("unplugin-element-plus/webpack")({
         // options
       }),
     ],
