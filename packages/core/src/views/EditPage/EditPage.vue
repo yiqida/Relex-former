a35066b55bda7734395b17ad75d20fffc3ff54b8
FDirectionClear <947288309@qq.com>
2023-04-15 16:32:05 +0800

    feat：全局asset-增加响应式计算屏幕垂直相对位置的scss函数

--- packages/core/src/views/EditPage/EditPage.vue
+++ packages/core/src/views/EditPage/EditPage.vue
@@ -3,7 +3,8 @@
     <div class="nav-header"></div>
     <div class="editor-container">
       <div class="left-comps-container"></div>
-      <div class="center-editor"></div>
+      <div class="center-editor">
+      </div>
       <div class="right-edited-container"></div>
     </div>
   </div>
@@ -15,10 +16,28 @@ export default {
 </script>
 
 <style lang="scss">
+$nav-height: 1rem;
+
 .editor-page-wrapper {
   border: 1px red solid;
   .nav-header {
-    height: 100px;
+    height: $nav-height;
+  }
+  .editor-container {
+    display: flex;
+    min-height: calc(100vh - $nav-height);
+    .left-comps-container {
+      border: 1px blueviolet solid;
+      width: 2.6rem;
+    }
+    .center-editor {
+      border: 1px hotpink solid;
+      width: 13.56rem;
+    }
+    .right-edited-container {
+      border: 1px forestgreen solid;
+      width: 3rem;
+    }
   }
 }
 </style>
