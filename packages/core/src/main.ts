e390bdc29d28f3567a625705b16e4aeb414b9ca2
wgf <843292747@qq.com>
2023-04-15 15:26:01 +0800

    fix: 引入tailwind

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "tailwindcss/tailwind.css"
+import './index.css'
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
 
