17027ec2aaab2bb89e8bfa754be553d033bbb82f
wgf <843292747@qq.com>
2023-05-04 19:19:06 +0800

    feat: 渲染item

---
+++ packages/core/src/views/EditPage/components/CopyAndDelete.vue
@@ -0,0 +1,68 @@
+<template>
+    <span class="drawing-item-copy" title="复制" @click="copyItem">
+        <el-icon>
+            <CopyDocument />
+        </el-icon>
+    </span>
+    <span class="drawing-item-delete" title="删除" @click="deleteItem">
+        <el-icon>
+            <Delete />
+        </el-icon>
+    </span>
+</template>
+
+<script setup lang="ts">
+import { ElIcon } from 'element-plus'
+import { CopyDocument, Delete } from '@element-plus/icons-vue'
+const emit = defineEmits(['copyItem', 'deleteItem'])
+const copyItem = () => {
+    emit('copyItem')
+}
+const deleteItem = () => {
+    emit('deleteItem')
+}
+</script>
+
+<style lang="scss" scoped>
+$lighterBlue: #409eff;
+
+.drawing-item-copy,
+.drawing-item-delete {
+    /* display: none; */
+    position: absolute;
+    top: -10px;
+    width: 22px;
+    height: 22px;
+    line-height: 22px;
+    text-align: center;
+    border-radius: 50%;
+    font-size: 12px;
+    border: 1px solid;
+    cursor: pointer;
+    z-index: 1;
+}
+
+.drawing-item-copy {
+    right: 32px;
+    border-color: $lighterBlue;
+    color: $lighterBlue;
+    background: #fff;
+
+    &:hover {
+        background: $lighterBlue;
+        color: #fff;
+    }
+}
+
+.drawing-item-delete {
+    right: 0px;
+    border-color: #f56c6c;
+    color: #f56c6c;
+    background: #fff;
+
+    &:hover {
+        background: #f56c6c;
+        color: #fff;
+    }
+}
+</style>
--- packages/core/src/views/EditPage/components/DraggableItem.vue
+++ packages/core/src/views/EditPage/components/DraggableItem.vue
@@ -1,39 +1,61 @@
 <template>
     <ElCol v-if="grid" :span="span" @click="handleActiveItem" :class="className">
         <component :is="def" :current-item="currentItem" layout="editor" :index="index"></component>
+        <CopyAndDelete />
     </ElCol>
     <div v-else @click="handleActiveItem" :class="className">
         <component :is="def" :current-item="currentItem" layout="editor" :index="index"></component>
+        <CopyAndDelete />
     </div>
 </template>
 <script setup lang="ts">
-import { ElCol } from "element-plus";
+import { ElCol } from 'element-plus'
+import CopyAndDelete from './CopyAndDelete.vue'
 const props = defineProps<{
-    currentItem: any,
-    activeId: number,
-    index: number,
+    currentItem: any
+    activeId: number
+    index: number
     drawingList: any[]
 }>()
 
 const emit = defineEmits(['activeItem'])
 
-const config = props.currentItem.__config__ as any;
-const {
-    needBorder,
-    formId,
-    grid,
-    span,
-    def
-} = config;
-let className = '';
+const config = props.currentItem.__config__ as any
+const { needBorder, formId, grid, span, def } = config
+let className = 'drawing-item'
+if (props.activeId === formId) {
+    className += ' active-from-item'
+}
 if (needBorder) {
-    className =
-        props.activeId === formId ? 'drawing-row-item active-from-item' : 'drawing-row-item';
-} else {
-    className = props.activeId === formId ? 'drawing-item active-from-item' : 'drawing-item';
+    className += ' drawing-row-item'
 }
 
 const handleActiveItem = () => {
     emit('activeItem', props.currentItem)
 }
 </script>
+
+<style lang="scss" scoped>
+
+.drawing-item {
+    position: relative;
+}
+.drawing-row-item {
+    cursor: move;
+    box-sizing: border-box;
+    border: 1px dashed rgba(0, 0, 0, 0.12);
+    border-radius: 3px;
+}
+
+.active-from-item {
+    &>.el-form-item {
+        background: #f6f7ff;
+        border-radius: 6px;
+    }
+
+    &>.drawing-item-copy,
+    &>.drawing-item-delete {
+        display: initial;
+    }
+}
+</style>
